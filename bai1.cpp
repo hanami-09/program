9658e9ca9bf1cd4eebcfa8896757a81546bd3733
hanami-09 <gktuanbg@gmail.com>
2025-08-22 10:55:34 +0700

    Add files via upload

---
+++ bai1.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ifstream cin("test.inp");
+    ofstream cout("test.out");
+    string s;
+    getline(cin, s);
+    cout << s;
+}
